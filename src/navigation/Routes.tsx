5f6c8c33bc42b06dc27eaeb35f8fd2b097b93304
Federico Giorgino <federicog.dev@gmail.com>
2022-05-02 12:46:18 +0200

    React Native Appearance + Themed Routes

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -1,15 +1,39 @@
-import React, { useContext } from "react";
+import React from "react";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
+
 import { AppStack } from "./AppStack";
+import { CustomDarkTheme, CustomLightTheme } from "../theme";
+import { useColorScheme } from "react-native-appearance";
 
 export const Routes = () => {
+  const scheme = useColorScheme();
+  const theme: string = "automatic";
+
+  let THEME;
+  let STATUS_BAR_STYLE;
+
+  if (theme === "automatic") {
+    if (scheme === "dark") {
+      THEME = CustomDarkTheme;
+      STATUS_BAR_STYLE = "light-content";
+    } else {
+      THEME = CustomLightTheme;
+      STATUS_BAR_STYLE = "dark-content";
+    }
+  } else if (theme === "light") {
+    THEME = CustomLightTheme;
+    STATUS_BAR_STYLE = "dark-content";
+  } else if (theme === "dark") {
+    THEME = CustomDarkTheme;
+    STATUS_BAR_STYLE = "light-content";
+  }
+
   return (
-    <>
-      <NavigationContainer>
-        <AppStack />
-      </NavigationContainer>
-      <StatusBar />
-    </>
+    <NavigationContainer theme={THEME}>
+      <AppStack />
+      {/** @ts-ignore **/}
+      <StatusBar barStyle={STATUS_BAR_STYLE} />
+    </NavigationContainer>
   );
 };
