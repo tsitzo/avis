c729c555e859b81a904730c510b2ecabff7317f7
Federico Giorgino <federicog.dev@gmail.com>
2022-06-14 12:13:07 +0200

    Deleted Category/SourceTile

--- src/screens/MyNewsScreen.tsx
+++ src/screens/MyNewsScreen.tsx
@@ -1,70 +1,14 @@
-import React, { useContext } from "react";
-import {
-  Dimensions,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { SettingsContext } from "../context/SettingsContext";
-
-import SafeArea from "../components/layout/SafeArea";
-import Typography from "../components/text/Typography";
-import CategoryTile from "../components/ui/CategoryTile";
-import { sources } from "../data/sources";
-import SourceTile from "../components/ui/SourceTile";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const MyNewsScreen = () => {
-  const { selectedCategories, selectedCountry } = useContext(SettingsContext);
-
   return (
-    <SafeArea>
-      <View style={{ flex: 1 }}>
-        <View style={styles.textWrapper}>
-          <Typography variant="bold" size={24}>
-            Categories
-          </Typography>
-        </View>
-        <FlatList
-          horizontal
-          data={selectedCategories}
-          renderItem={({ item }) => (
-            <TouchableOpacity style={styles.categoryWrapper}>
-              <CategoryTile category={item} />
-            </TouchableOpacity>
-          )}
-        />
-        <View style={styles.textWrapper}>
-          <Typography variant="bold" size={24}>
-            Sources
-          </Typography>
-        </View>
-        <FlatList
-          style={{ marginBottom: 20 }}
-          horizontal
-          data={sources.filter(
-            (source) => source.country === selectedCountry.iso
-          )}
-          renderItem={({ item }) => (
-            <TouchableOpacity style={styles.categoryWrapper}>
-              <SourceTile source={item} />
-            </TouchableOpacity>
-          )}
-        />
-      </View>
-    </SafeArea>
+    <View>
+      <Text>MyNewsScreen</Text>
+    </View>
   );
 };
 
 export default MyNewsScreen;
 
-const styles = StyleSheet.create({
-  categoryWrapper: {
-    width: Dimensions.get("window").width / 2 - 10,
-
-    padding: 5,
-  },
-  textWrapper: { padding: 10 },
-});
+const styles = StyleSheet.create({});
